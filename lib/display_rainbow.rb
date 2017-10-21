78c6bcf1aa6f0b43dd3aea24f09dc5caf5cbaad1
Lisa Mae <lisamae135b7@me.com>
2017-10-21 23:07:06 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
   puts "#{colors[0][0].upcase}: #{colors[0]}"
-  puts "#{colors[1][0].upcase}: #{colors[1]}"
+puts "#{colors[1][0].upcase}: #{colors[1]}"
   puts "#{colors[2][0].upcase}: #{colors[2]}"
     puts "#{colors[3][0].upcase}: #{colors[3]}"
       puts "#{colors[4][0].upcase}: #{colors[4]}"
