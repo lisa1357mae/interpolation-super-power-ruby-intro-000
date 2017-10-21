d516fe76638228cb8ddbe4a66b6ee35651d5faaf
Lisa Mae <lisamae135b7@me.com>
2017-10-21 23:14:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,5 +6,5 @@ print  "#{colors[2][0].upcase}: #{colors[2]}, "
   print "#{colors[3][0].upcase}: #{colors[3]}, "
   print "#{colors[4][0].upcase}: #{colors[4]}, "
       print "#{colors[5][0].upcase}: #{colors[5]}, "
-    print  "#{colors[6][0].upcase}: #{colors[6]}"
+    puts  "#{colors[6][0].upcase}: #{colors[6]}"
 end
