d5bbd6c1ff41734137e0e1c20bf8396976f19d3a
Lisa Mae <lisamae135b7@me.com>
2017-10-21 22:52:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 # Write your #display_rainbow method here
 def display_rainbow (color)
-  puts ": #{{color[0], }}"
+  puts ": #{{color[0] }}"
   letters = ["b", "a", "c"]
   puts
   colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet' ]
